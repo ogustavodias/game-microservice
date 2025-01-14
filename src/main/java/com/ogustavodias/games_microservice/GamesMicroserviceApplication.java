6460cfdde829e8d9b0b13d1811c237c2f14860ea
ogustavodias <gustavoalvesdiaz08@gmail.com>
2025-01-14 20:22:23 -0300

    first commit

---
+++ src/main/java/com/ogustavodias/games_microservice/GamesMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ogustavodias.games_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ogustavodias/games_microservice/GamesMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogustavodias.games_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
